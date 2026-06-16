fe7461e00282ac0c361aef5cfbbfbe8b4255c189
Adithyan <adithyanca632>
2026-06-16 16:12:41 +0530

    changed statebasics

--- src/component/StateBasics.jsx
+++ src/component/StateBasics.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Statebasics = () => {
   return (
     <div>
-      <h1 className='text-3xl font-bold'>Welcome</h1>
+      <h1 className='text-3xl font-bold'>Welcome git</h1>
     </div>
   )
 }
